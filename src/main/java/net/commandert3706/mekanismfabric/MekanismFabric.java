aad0cd5bf1c604c6a51fd90ac7b88ffcebdd613a
Commandert3706 <thileshan@outlook.com>
2023-05-27 09:05:40 +0200

    Initial Commit

---
+++ src/main/java/net/commandert3706/mekanismfabric/MekanismFabric.java
@@ -0,0 +1,15 @@
+package net.commandert3706.mekanismfabric;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MekanismFabric implements ModInitializer {
+	public static final String MOD_ID = "mekanismfabric";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/commandert3706/mekanismfabric/MekanismFabricClient.java
@@ -0,0 +1,10 @@
+package net.commandert3706.mekanismfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MekanismFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
